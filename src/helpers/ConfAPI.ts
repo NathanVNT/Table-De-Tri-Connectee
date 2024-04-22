b5e1e0b6ca3eb086851b574abae0c92bcfcaf72d
Nathan Vernet <nathan.vernet@lyceeastier.com>
2024-04-22 16:08:39 +0200

    Modif conf api

--- src/helpers/ConfAPI.ts
+++ src/helpers/ConfAPI.ts
@@ -1,6 +1,6 @@
 const config = {
     api: {
-        ip: 'http://192.168.1.254', // Remplacez par l'IP de votre API
+        ip: 'http://92.137.84.43', // Remplacez par l'IP de votre API
         port: '3000', // Remplacez par le port de votre API
     },
 };
