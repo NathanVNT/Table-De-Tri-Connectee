e9edd09f7fea198b25913f3ff0851f8c327d6e54
Nathan Vernet <nathan.vernet@lyceeastier.com>
2024-05-31 16:55:28 +0200

    Revert "Delete src/helpers/ConfAPI.ts"
    This reverts commit 3889f4e0402e096f020cad495cb4bcf7b1a1cebc.

---
+++ src/helpers/ConfAPI.ts
@@ -0,0 +1,8 @@
+const config = {
+    api: {
+        ip: 'http://192.168.1.253', // Remplacez par l'IP de votre API
+        port: '3000', // Remplacez par le port de votre API
+    },
+};
+
+export default config;
